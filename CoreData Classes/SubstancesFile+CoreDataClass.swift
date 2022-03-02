95af445492648eddb5e7826c02fe8bd04b67d6fd
Isaak <isaak.hanimann@gmail.com>
2022-03-02 20:50:54 +0100

    Clean up parsing code

--- CoreData Classes/SubstancesFile+CoreDataClass.swift
+++ CoreData Classes/SubstancesFile+CoreDataClass.swift
@@ -11,7 +11,9 @@ public class SubstancesFile: NSManagedObject, Decodable {
         case notEnoughSubstancesParsed
     }
 
-    var substances: [Substance] = []
+    private var substancesForParsing: [Substance] = []
+    private var contextForParsing: NSManagedObjectContext!
+    private var unresolvedsForParsing = Set<UnresolvedInteraction>()
 
     required convenience public init(from decoder: Decoder) throws {
         guard let context = decoder.userInfo[CodingUserInfoKey.managedObjectContext] as? NSManagedObjectContext else {
@@ -26,22 +28,23 @@ public class SubstancesFile: NSManagedObject, Decodable {
             throw DecodingError.notEnoughSubstancesParsed
         }
         self.init(context: context)
-        self.substances = substances
-        goThroughSubstancesOneByOneAndCreateObjectsAndRelationships(context: context)
+        self.substancesForParsing = substances
+        self.contextForParsing = context
+        goThroughSubstancesOneByOneAndCreateObjectsAndRelationships()
     }
 
-    private func goThroughSubstancesOneByOneAndCreateObjectsAndRelationships(context: NSManagedObjectContext) {
-        createClasses(context: context)
-        createEffectsAndAddThemToSubstances(context: context)
+    private func goThroughSubstancesOneByOneAndCreateObjectsAndRelationships() {
+        createClasses()
+        createEffectsAndAddThemToSubstances()
         // The following 2 methods must be called after the classes have been constructed
         createCrossTolerances()
-        createInteractions(context: context)
+        createInteractions()
     }
 
-    private func createClasses(context: NSManagedObjectContext) {
+    private func createClasses() {
         var psychoactives = Set<PsychoactiveClass>()
         var chemicals = Set<ChemicalClass>()
-        for substance in substances {
+        for substance in substancesForParsing {
             for psychoactiveName in substance.decodedClasses?.psychoactive ?? [] {
                 let match = psychoactives.first { cat in
                     cat.nameUnwrapped.lowercased() == psychoactiveName.lowercased()
@@ -49,7 +52,7 @@ public class SubstancesFile: NSManagedObject, Decodable {
                 if let matchUnwrapped = match {
                     matchUnwrapped.addToSubstances(substance)
                 } else {
-                    let pClass = PsychoactiveClass(context: context)
+                    let pClass = PsychoactiveClass(context: contextForParsing)
                     pClass.name = psychoactiveName
                     pClass.addToSubstances(substance)
                     psychoactives.insert(pClass)
@@ -62,7 +65,7 @@ public class SubstancesFile: NSManagedObject, Decodable {
                 if let matchUnwrapped = match {
                     matchUnwrapped.addToSubstances(substance)
                 } else {
-                    let cClass = ChemicalClass(context: context)
+                    let cClass = ChemicalClass(context: contextForParsing)
                     cClass.name = chemicalName
                     cClass.addToSubstances(substance)
                     chemicals.insert(cClass)
@@ -73,9 +76,9 @@ public class SubstancesFile: NSManagedObject, Decodable {
         self.chemicalClasses = chemicals as NSSet
     }
 
-    private func createEffectsAndAddThemToSubstances(context: NSManagedObjectContext) {
+    private func createEffectsAndAddThemToSubstances() {
         var effects = Set<Effect>()
-        for substance in substances {
+        for substance in substancesForParsing {
             for decodedEff in substance.decodedEffects {
                 let match = effects.first { eff in
                     eff.nameUnwrapped.hasEqualMeaning(other: decodedEff.name)
@@ -83,7 +86,7 @@ public class SubstancesFile: NSManagedObject, Decodable {
                 if let matchUnwrapped = match {
                     substance.addToEffects(matchUnwrapped)
                 } else {
-                    let newEffect = Effect(context: context)
+                    let newEffect = Effect(context: contextForParsing)
                     newEffect.name = decodedEff.name.capitalized
                     newEffect.url = decodedEff.url
                     effects.insert(newEffect)
@@ -94,7 +97,7 @@ public class SubstancesFile: NSManagedObject, Decodable {
     }
 
     private func createCrossTolerances() {
-        for substance in substances {
+        for substance in substancesForParsing {
             for toleranceName in substance.decodedCrossTolerances {
                 // check if psychoactive
                 let matchPsycho = self.psychoactiveClassesUnwrapped.first { psy in
@@ -113,7 +116,7 @@ public class SubstancesFile: NSManagedObject, Decodable {
                     continue
                 }
                 // check if substance
-                let matchSubstance = substances.first { sub in
+                let matchSubstance = substancesForParsing.first { sub in
                     sub.nameUnwrapped.hasEqualMeaning(other: toleranceName)
                 }
                 if let matchUnwrapped = matchSubstance {
@@ -124,32 +127,23 @@ public class SubstancesFile: NSManagedObject, Decodable {
         }
     }
 
-    private func createInteractions(context: NSManagedObjectContext) {
-        var unresolvedInteractions = Set<UnresolvedInteraction>()
-        for substance in substances {
+    private func createInteractions() {
+        unresolvedsForParsing = Set<UnresolvedInteraction>()
+        for substance in substancesForParsing {
             let uncertainNames = substance.decodedUncertain.map { $0.name }
-            unresolvedInteractions = addToPsychoactivesChemicalsSubstancesOrUnresolved(
-                substance: substance,
-                interactionNames: uncertainNames,
-                interactionType: .uncertain,
-                unresolvedsFromBefore: unresolvedInteractions,
-                context: context
+            addToPsychoactivesChemicalsSubstancesOrUnresolved(
+                addToSub: AddToUncertainSubstance(substance: substance),
+                interactionNames: uncertainNames
             )
             let unsafeNames = substance.decodedUnsafe.map { $0.name }
-            unresolvedInteractions = addToPsychoactivesChemicalsSubstancesOrUnresolved(
-                substance: substance,
-                interactionNames: unsafeNames,
-                interactionType: .unsafe,
-                unresolvedsFromBefore: unresolvedInteractions,
-                context: context
+            addToPsychoactivesChemicalsSubstancesOrUnresolved(
+                addToSub: AddToUnsafeSubstance(substance: substance),
+                interactionNames: unsafeNames
             )
             let dangerousNames = substance.decodedDangerous.map { $0.name }
-            unresolvedInteractions = addToPsychoactivesChemicalsSubstancesOrUnresolved(
-                substance: substance,
-                interactionNames: dangerousNames,
-                interactionType: .dangerous,
-                unresolvedsFromBefore: unresolvedInteractions,
-                context: context
+            addToPsychoactivesChemicalsSubstancesOrUnresolved(
+                addToSub: AddToDangerousSubstance(substance: substance),
+                interactionNames: dangerousNames
             )
         }
     }
@@ -158,30 +152,17 @@ public class SubstancesFile: NSManagedObject, Decodable {
         case uncertain, unsafe, dangerous
     }
 
-    // swiftlint:disable cyclomatic_complexity
-    // swiftlint:disable function_body_length
     private func addToPsychoactivesChemicalsSubstancesOrUnresolved(
-        substance: Substance,
-        interactionNames: [String],
-        interactionType: InteractionType,
-        unresolvedsFromBefore: Set<UnresolvedInteraction>,
-        context: NSManagedObjectContext
-    ) -> Set<UnresolvedInteraction> {
-        var newUnresolveds = unresolvedsFromBefore
+        addToSub: AddToSubstance,
+        interactionNames: [String]
+    ) {
         for interactionName in interactionNames {
             // check if psychoactive
             let matchPsycho = self.psychoactiveClassesUnwrapped.first { psy in
                 psy.nameUnwrapped.hasEqualMeaning(other: interactionName)
             }
             if let psyUnwrap = matchPsycho {
-                switch interactionType {
-                case .uncertain:
-                    substance.addToUncertainPsychoactives(psyUnwrap)
-                case .unsafe:
-                    substance.addToUnsafePsychoactives(psyUnwrap)
-                case .dangerous:
-                    substance.addToDangerousPsychoactives(psyUnwrap)
-                }
+                addToSub.addToPsychoactives(psychocative: psyUnwrap)
                 continue
             }
             // check if chemical
@@ -189,60 +170,31 @@ public class SubstancesFile: NSManagedObject, Decodable {
                 chem.nameUnwrapped.hasEqualMeaning(other: interactionName)
             }
             if let chemUnwrap = matchChemical {
-                switch interactionType {
-                case .uncertain:
-                    substance.addToUncertainChemicals(chemUnwrap)
-                case .unsafe:
-                    substance.addToUnsafeChemicals(chemUnwrap)
-                case .dangerous:
-                    substance.addToDangerousChemicals(chemUnwrap)
-                }
+                addToSub.addToChemicals(chemical: chemUnwrap)
                 continue
             }
             // check if substance
-            let matchSub = self.substances.first { sub in
+            let matchSub = self.substancesForParsing.first { sub in
                 sub.nameUnwrapped.hasEqualMeaning(other: interactionName)
             }
             if let subUnwrap = matchSub {
-                switch interactionType {
-                case .uncertain:
-                    substance.addToUncertainSubstances(subUnwrap)
-                case .unsafe:
-                    substance.addToUnsafeSubstances(subUnwrap)
-                case .dangerous:
-                    substance.addToDangerousSubstances(subUnwrap)
-                }
+                addToSub.addToSubstances(substance: subUnwrap)
                 continue
             }
             // if still here there was no match
             // check if there are already unresolved interactions
-            let unrMatch = unresolvedsFromBefore.first { unr in
+            let unrMatch = unresolvedsForParsing.first { unr in
                 unr.nameUnwrapped.hasEqualMeaning(other: interactionName)
             }
             if let unrUnwrap = unrMatch {
-                switch interactionType {
-                case .uncertain:
-                    unrUnwrap.addToUncertainSubstances(substance)
-                case .unsafe:
-                    unrUnwrap.addToUnsafeSubstances(substance)
-                case .dangerous:
-                    unrUnwrap.addToDangerousSubstances(substance)
-                }
+                addToSub.addToUnresolved(unresolved: unrUnwrap)
             } else {
-                let newUnresolved = UnresolvedInteraction(context: context)
+                let newUnresolved = UnresolvedInteraction(context: contextForParsing)
                 newUnresolved.name = interactionName.capitalized
-                switch interactionType {
-                case .uncertain:
-                    substance.addToUncertainUnresolveds(newUnresolved)
-                case .unsafe:
-                    substance.addToUnsafeUnresolveds(newUnresolved)
-                case .dangerous:
-                    substance.addToDangerousUnresolveds(newUnresolved)
-                }
-                newUnresolveds.insert(newUnresolved)
+                addToSub.addToUnresolved(unresolved: newUnresolved)
+                unresolvedsForParsing.insert(newUnresolved)
             }
         }
-        return newUnresolveds
     }
 
     static let namesOfUncontrolledSubstances = [
@@ -251,4 +203,74 @@ public class SubstancesFile: NSManagedObject, Decodable {
         "Choline bitartrate",
         "Citicoline"
     ]
+}
+
+private protocol AddToSubstance {
+    func addToChemicals(chemical: ChemicalClass)
+    func addToPsychoactives(psychocative: PsychoactiveClass)
+    func addToSubstances(substance: Substance)
+    func addToUnresolved(unresolved: UnresolvedInteraction)
+}
+
+private struct AddToUncertainSubstance: AddToSubstance {
+
+    let substance: Substance
+
+    func addToChemicals(chemical: ChemicalClass) {
+        substance.addToUncertainChemicals(chemical)
+    }
+
+    func addToPsychoactives(psychocative: PsychoactiveClass) {
+        substance.addToUncertainPsychoactives(psychocative)
+    }
+
+    func addToSubstances(substance: Substance) {
+        substance.addToUncertainSubstances(substance)
+    }
+
+    func addToUnresolved(unresolved: UnresolvedInteraction) {
+        substance.addToUncertainUnresolveds(unresolved)
+    }
+}
+
+private struct AddToUnsafeSubstance: AddToSubstance {
+
+    let substance: Substance
+
+    func addToChemicals(chemical: ChemicalClass) {
+        substance.addToUnsafeChemicals(chemical)
+    }
+
+    func addToPsychoactives(psychocative: PsychoactiveClass) {
+        substance.addToUnsafePsychoactives(psychocative)
+    }
+
+    func addToSubstances(substance: Substance) {
+        substance.addToUnsafeSubstances(substance)
+    }
+
+    func addToUnresolved(unresolved: UnresolvedInteraction) {
+        substance.addToUnsafeUnresolveds(unresolved)
+    }
+}
+
+private struct AddToDangerousSubstance: AddToSubstance {
+
+    let substance: Substance
+
+    func addToChemicals(chemical: ChemicalClass) {
+        substance.addToDangerousChemicals(chemical)
+    }
+
+    func addToPsychoactives(psychocative: PsychoactiveClass) {
+        substance.addToDangerousPsychoactives(psychocative)
+    }
+
+    func addToSubstances(substance: Substance) {
+        substance.addToDangerousSubstances(substance)
+    }
+
+    func addToUnresolved(unresolved: UnresolvedInteraction) {
+        substance.addToDangerousUnresolveds(unresolved)
+    }
 }
