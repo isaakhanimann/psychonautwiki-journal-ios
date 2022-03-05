0578ed7b0383a86c8825b2c039f39f869e711f3e
Isaak <isaak.hanimann@gmail.com>
2022-03-05 12:06:28 +0100

    Test that decoding deletes old file

--- JournalTests/FileTests.swift
+++ JournalTests/FileTests.swift
@@ -20,4 +20,12 @@ class FileTests: XCTestCase {
         XCTAssertEqual(files.count, 1)
     }
 
+    func testHasOneFileAfterDecoding() async throws {
+        let fileData = getInitialData()
+        try await PersistenceController.preview.decodeAndSaveFile(from: fileData)
+        let fetchRequest = SubstancesFile.fetchRequest()
+        let files = try PersistenceController.preview.viewContext.fetch(fetchRequest)
+        XCTAssertEqual(files.count, 1)
+    }
+
 }
