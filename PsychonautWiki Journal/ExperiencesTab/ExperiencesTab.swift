859d468f9248d00f9a73606944c434bb1e8128a1
Isaak <isaak.hanimann@gmail.com>
2022-03-06 17:14:34 +0100

    Make experiences tab same style as search tab

--- PsychonautWiki Journal/ExperiencesTab/ExperiencesTab.swift
+++ PsychonautWiki Journal/ExperiencesTab/ExperiencesTab.swift
@@ -25,6 +25,7 @@ struct ExperiencesTab: View {
                         }
                     }
                 }
+                .listStyle(.insetGrouped)
                 if !viewModel.hasExperiences {
                     Button(action: {
                         withAnimation {
