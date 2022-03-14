07bc9ba8deffe1f3d8660ded1402d708a6112b32
Isaak <isaak.hanimann@gmail.com>
2022-03-14 10:20:25 +0100

    Create FAQ section

--- PsychonautWiki Journal/SettingsTab/FAQ/FAQView.swift
+++ PsychonautWiki Journal/SettingsTab/FAQ/FAQView.swift
@@ -1,30 +1,15 @@
 import SwiftUI
 
 struct FAQView: View {
-    @State private var selection: Set<QuestionAndAnswer> = []
 
     var body: some View {
-        NavigationView {
-            ScrollView {
-                ForEach(QuestionAndAnswer.list) { qAndA in
-                    QandAView(questionAndAnswer: qAndA, isExpanded: self.selection.contains(qAndA))
-                        .onTapGesture { self.selectDeselect(qAndA) }
-                    Divider()
-                }
-                .padding(.horizontal)
-            }
-            .navigationTitle("FAQ")
-        }
-    }
-
-    private func selectDeselect(_ qAndA: QuestionAndAnswer) {
-        withAnimation {
-            if selection.contains(qAndA) {
-                selection.remove(qAndA)
-            } else {
-                selection.insert(qAndA)
+        List(QuestionAndAnswer.list) { qAndA in
+            DisclosureGroup(qAndA.question) {
+                Text(qAndA.answer)
+                    .foregroundColor(.secondary)
             }
         }
+        .navigationTitle("FAQ")
     }
 }
 
--- PsychonautWiki Journal/SettingsTab/FAQ/QuestionAndAnswer.swift
+++ PsychonautWiki Journal/SettingsTab/FAQ/QuestionAndAnswer.swift
@@ -10,46 +10,37 @@ struct QuestionAndAnswer: Identifiable, Hashable {
     static let list: [QuestionAndAnswer] = [
         QuestionAndAnswer(
             question: "How is the timeline drawn?",
-            answer: "Based on onset, comeup, peak and offset"
+            answer: "The onset duration range from PsychonautWiki defines when the curve starts going up, the comeup how long it goes up for, the peak how long it stays up and the offset how long it takes to come down to baseline. The peak and offset durations are linearly interpolated based on the dose if possible, else it just chooses the middle value of the range. If not all of these durations are defined the timeline for this substance cannot be drawn."
         ),
         QuestionAndAnswer(
-            question: "How can I change the timeline?",
-            answer: "By changing the PsychonautWiki article"
-        ),
-        QuestionAndAnswer(
-            question: "How can I change the dose info?",
-            answer: "By changing the PsychonautWiki article"
+            question: "How can one change or add info on a substance (duration, dose, interactions and effects)?",
+            answer: "By editing the corresponding PsychonautWiki article."
         ),
         QuestionAndAnswer(
             question: "Why does the timeline not add up the curves for the same substance?",
-            answer: "Because its complex"
+            answer: "One can not add two curves together because one ingestion might build up tolerance, influencing the curve of the other ingestion. The curve can only be drawn based on data that is available through PsychonautWiki."
         ),
         QuestionAndAnswer(
             question: "Why does the timeline not show cumulative effects of different substances?",
-            answer: "Because its complex"
+            answer: "Because the curve can only be drawn based on data that is available through PsychonautWiki."
         ),
         QuestionAndAnswer(
             question: "Does PsychonautWiki Journal add up the doses of the same substance?",
             answer: "No, it treats every ingestion independently. So make sure that your cumulative dose is not dangerous"
         ),
         QuestionAndAnswer(
-            question: "Why would I want to store the experiences in the calendar?",
-            answer: "In case anything goes wrong with the app in the future"
-        ),
-        QuestionAndAnswer(
-            question: "How often is the data updated?",
-            answer: "Every 2 weeks but you can tap reload in settings if you want to trigger an update"
+            question: "How often are substances updated?",
+            answer: "The app automatically tries to update its substances every 4 days but you can tap refresh in settings if you want it to update immediately."
         ),
         QuestionAndAnswer(
             question: "Why is the eye closed by default?",
             answer: """
-    To make sure that only users that have added a substance through one of the links in the PsychonautWiki articles can find the substances they expect.
-    People who find the app only through the app store only see a limited set of substances.
+    Apple reviewers think that having information on illicit substances in the app by default is encouraging their use. So if users don't opt in they only see a limited set of substances.
     """
         ),
         QuestionAndAnswer(
-            question: "How can I open the eye?",
-            answer: "By triple tapping it"
+            question: "How can one open the eye?",
+            answer: "By triple tapping it."
         )
     ]
 }
