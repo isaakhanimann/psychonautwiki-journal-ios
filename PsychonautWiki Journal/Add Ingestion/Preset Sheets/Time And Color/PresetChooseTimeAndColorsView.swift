187f9c81d87f548a40e64f378298ac12b642217f
Isaak <isaak.hanimann@gmail.com>
2022-03-28 23:43:55 +0200

    Let the user know that the sheet must be dismissed manually if SwiftUI bug occurs

--- PsychonautWiki Journal/Add Ingestion/Preset Sheets/Time And Color/PresetChooseTimeAndColorsView.swift
+++ PsychonautWiki Journal/Add Ingestion/Preset Sheets/Time And Color/PresetChooseTimeAndColorsView.swift
@@ -8,71 +8,85 @@ struct PresetChooseTimeAndColorsView: View {
     @EnvironmentObject private var sheetViewModel: SheetViewModel
     @EnvironmentObject private var toastViewModel: ToastViewModel
     @StateObject private var viewModel = ViewModel()
+    @State private var hasPressedDismiss = false
 
     var body: some View {
-        ZStack(alignment: .bottom) {
-            Form {
-                Section("Choose Time") {
-                    DatePicker(
-                        "Time",
-                        selection: $viewModel.selectedTime,
-                        displayedComponents: [.date, .hourAndMinute]
-                    )
-                    .labelsHidden()
-                }
-                ForEach($viewModel.componentColorCombos) { $combo in
-                    Section("\(combo.component.substanceNameUnwrapped) Color") {
-                        ColorPicker(selectedColor: $combo.selectedColor)
+        if !hasPressedDismiss {
+            ZStack(alignment: .bottom) {
+                Form {
+                    Section("Choose Time") {
+                        DatePicker(
+                            "Time",
+                            selection: $viewModel.selectedTime,
+                            displayedComponents: [.date, .hourAndMinute]
+                        )
+                        .labelsHidden()
                     }
-                }
-                EmptySectionForPadding()
-            }
-            if let experienceUnwrap = addIngestionContext.experience {
-                Button("Add Ingestion") {
-                    viewModel.addIngestions(to: experienceUnwrap)
-                    sheetViewModel.dismiss()
-                    toastViewModel.showSuccessToast()
-                }
-                .buttonStyle(.primary)
-                .padding()
-            } else {
-                VStack {
-                    let twoDaysAgo = Date().addingTimeInterval(-2*24*60*60)
-                    if let lastExperienceUnwrap = viewModel.lastExperience,
-                       lastExperienceUnwrap.dateForSorting > twoDaysAgo {
-                        Button("Add to \(lastExperienceUnwrap.titleUnwrapped)") {
-                            viewModel.addIngestions(to: lastExperienceUnwrap)
-                            sheetViewModel.dismiss()
-                            toastViewModel.showSuccessToast()
+                    ForEach($viewModel.componentColorCombos) { $combo in
+                        Section("\(combo.component.substanceNameUnwrapped) Color") {
+                            ColorPicker(selectedColor: $combo.selectedColor)
                         }
-                        .padding(.horizontal)
                     }
-                    Button("Add to New Experience") {
-                        viewModel.addIngestionsToNewExperience()
-                        sheetViewModel.dismiss()
+                    EmptySectionForPadding()
+                }
+                if let experienceUnwrap = addIngestionContext.experience {
+                    Button("Add Ingestion") {
+                        viewModel.addIngestions(to: experienceUnwrap)
+                        dismissSheet()
                         toastViewModel.showSuccessToast()
                     }
+                    .buttonStyle(.primary)
                     .padding()
+                } else {
+                    VStack {
+                        let twoDaysAgo = Date().addingTimeInterval(-2*24*60*60)
+                        if let lastExperienceUnwrap = viewModel.lastExperience,
+                           lastExperienceUnwrap.dateForSorting > twoDaysAgo {
+                            Button("Add to \(lastExperienceUnwrap.titleUnwrapped)") {
+                                viewModel.addIngestions(to: lastExperienceUnwrap)
+                                dismissSheet()
+                                toastViewModel.showSuccessToast()
+                            }
+                            .padding(.horizontal)
+                        }
+                        Button("Add to New Experience") {
+                            viewModel.addIngestionsToNewExperience()
+                            dismissSheet()
+                            toastViewModel.showSuccessToast()
+                        }
+                        .padding()
+                    }
+                    .buttonStyle(.primary)
                 }
-                .buttonStyle(.primary)
             }
-        }
-        .task {
-            viewModel.presetDose = dose
-            if addIngestionContext.experience == nil {
-                viewModel.lastExperience = PersistenceController.shared.getLatestExperience()
-            }
-            viewModel.componentColorCombos = preset.componentsUnwrapped.map { com in
-                ComponentColorCombo(component: com)
-            }
-        }
-        .navigationBarTitle("Ingestion Time")
-        .toolbar {
-            ToolbarItem(placement: .navigationBarTrailing) {
-                Button("Cancel") {
-                    sheetViewModel.dismiss()
+            .task {
+                viewModel.presetDose = dose
+                if addIngestionContext.experience == nil {
+                    viewModel.lastExperience = PersistenceController.shared.getLatestExperience()
+                }
+                viewModel.componentColorCombos = preset.componentsUnwrapped.map { com in
+                    ComponentColorCombo(component: com)
                 }
             }
+            .navigationBarTitle("Ingestion Time")
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button("Cancel") {
+                        dismissSheet()
+                    }
+                }
+            }
+        } else {
+            Text("Please Swipe Down To Dismiss")
+        }
+    }
+
+    private func dismissSheet() {
+        sheetViewModel.dismiss()
+        // this is here because there is a SwiftUI bug that sometimes prevents the sheet from being dismissable
+        // the delay makes sure that one can't see the UI change in case the sheet is being dismissed
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+            hasPressedDismiss = true
         }
     }
 }
--- PsychonautWiki Journal/Add Ingestion/Substance Sheets/Time and Color/ChooseTimeAndColor.swift
+++ PsychonautWiki Journal/Add Ingestion/Substance Sheets/Time and Color/ChooseTimeAndColor.swift
@@ -10,69 +10,83 @@ struct ChooseTimeAndColor: View {
     @EnvironmentObject private var toastViewModel: ToastViewModel
     @EnvironmentObject private var addIngestionContext: AddIngestionSheetContext
     @StateObject var viewModel = ViewModel()
+    @State private var hasPressedDismiss = false
 
     var body: some View {
-        ZStack(alignment: .bottom) {
-            Form {
-                Section("Choose Time") {
-                    DatePicker(
-                        "Time",
-                        selection: $viewModel.selectedTime,
-                        displayedComponents: [.date, .hourAndMinute]
-                    )
-                    .labelsHidden()
-                }
-                Section("Choose Color") {
-                    ColorPicker(selectedColor: $viewModel.selectedColor)
-                }
-            }
-            if let experienceUnwrap = addIngestionContext.experience {
-                Button("Add Ingestion") {
-                    viewModel.addIngestion(to: experienceUnwrap)
-                    sheetViewModel.dismiss()
-                    toastViewModel.showSuccessToast()
-                }
-                .buttonStyle(.primary)
-                .padding()
-            } else {
-                VStack {
-                    let twoDaysAgo = Date().addingTimeInterval(-2*24*60*60)
-                    if let lastExperienceUnwrap = viewModel.lastExperience,
-                       lastExperienceUnwrap.dateForSorting > twoDaysAgo {
-                        Button("Add to \(lastExperienceUnwrap.titleUnwrapped)") {
-                            viewModel.addIngestion(to: lastExperienceUnwrap)
-                            sheetViewModel.dismiss()
-                            toastViewModel.showSuccessToast()
-                        }
-                        .padding(.horizontal)
+        if !hasPressedDismiss {
+            ZStack(alignment: .bottom) {
+                Form {
+                    Section("Choose Time") {
+                        DatePicker(
+                            "Time",
+                            selection: $viewModel.selectedTime,
+                            displayedComponents: [.date, .hourAndMinute]
+                        )
+                        .labelsHidden()
                     }
-                    Button("Add to New Experience") {
-                        viewModel.addIngestionToNewExperience()
-                        sheetViewModel.dismiss()
+                    Section("Choose Color") {
+                        ColorPicker(selectedColor: $viewModel.selectedColor)
+                    }
+                }
+                if let experienceUnwrap = addIngestionContext.experience {
+                    Button("Add Ingestion") {
+                        viewModel.addIngestion(to: experienceUnwrap)
+                        dismissSheet()
                         toastViewModel.showSuccessToast()
                     }
+                    .buttonStyle(.primary)
                     .padding()
+                } else {
+                    VStack {
+                        let twoDaysAgo = Date().addingTimeInterval(-2*24*60*60)
+                        if let lastExperienceUnwrap = viewModel.lastExperience,
+                           lastExperienceUnwrap.dateForSorting > twoDaysAgo {
+                            Button("Add to \(lastExperienceUnwrap.titleUnwrapped)") {
+                                viewModel.addIngestion(to: lastExperienceUnwrap)
+                                dismissSheet()
+                                toastViewModel.showSuccessToast()
+                            }
+                            .padding(.horizontal)
+                        }
+                        Button("Add to New Experience") {
+                            viewModel.addIngestionToNewExperience()
+                            dismissSheet()
+                            toastViewModel.showSuccessToast()
+                        }
+                        .padding()
+                    }
+                    .buttonStyle(.primary)
                 }
-                .buttonStyle(.primary)
             }
-        }
-        .task {
-            viewModel.substance = substance
-            viewModel.administrationRoute = administrationRoute
-            viewModel.dose = dose ?? 0
-            viewModel.units = units
-            if addIngestionContext.experience == nil {
-                viewModel.lastExperience = PersistenceController.shared.getLatestExperience()
+            .task {
+                viewModel.substance = substance
+                viewModel.administrationRoute = administrationRoute
+                viewModel.dose = dose ?? 0
+                viewModel.units = units
+                if addIngestionContext.experience == nil {
+                    viewModel.lastExperience = PersistenceController.shared.getLatestExperience()
+                }
+                viewModel.setDefaultColor()
             }
-            viewModel.setDefaultColor()
-        }
-        .navigationBarTitle("Ingestion Time")
-        .toolbar {
-            ToolbarItem(placement: .navigationBarTrailing) {
-                Button("Cancel") {
-                    sheetViewModel.dismiss()
+            .navigationBarTitle("Ingestion Time")
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button("Cancel") {
+                        dismissSheet()
+                    }
                 }
             }
+        } else {
+            Text("Please Swipe Down To Dismiss")
+        }
+    }
+
+    private func dismissSheet() {
+        sheetViewModel.dismiss()
+        // this is here because there is a SwiftUI bug that sometimes prevents the sheet from being dismissable
+        // the delay makes sure that one can't see the UI change in case the sheet is being dismissed
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+            hasPressedDismiss = true
         }
     }
 }
