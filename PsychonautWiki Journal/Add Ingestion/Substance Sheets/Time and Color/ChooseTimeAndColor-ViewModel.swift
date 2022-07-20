fc9a803570427980a8f81a3a8b8ab4b3be263be2
Isaak <isaak.hanimann@gmail.com>
2022-07-20 13:13:43 +0200

    Add substance companion when adding ingestion

--- PsychonautWiki Journal/Add Ingestion/Substance Sheets/Time and Color/ChooseTimeAndColor-ViewModel.swift
+++ PsychonautWiki Journal/Add Ingestion/Substance Sheets/Time and Color/ChooseTimeAndColor-ViewModel.swift
@@ -5,59 +5,55 @@ extension ChooseTimeAndColor {
 
     class ViewModel: ObservableObject {
 
-        @Published var lastExperience: Experience?
         @Published var selectedColor = SubstanceColor.allCases.randomElement() ?? SubstanceColor.blue
         @Published var selectedTime = Date()
+        @Published var isLoadingCompanions = true
+        var doesCompanionExistAlready = true
         var substance: Substance?
         var administrationRoute = AdministrationRoute.allCases.randomElement() ?? AdministrationRoute.oral
         var dose: Double = 0
         var units: String?
 
         func setDefaultColor() {
-            let fetchRequest: NSFetchRequest<Ingestion> = Ingestion.fetchRequest()
-            fetchRequest.sortDescriptors = [ NSSortDescriptor(keyPath: \Ingestion.time, ascending: false) ]
+            let fetchRequest = SubstanceCompanion.fetchRequest()
             guard let name = substance?.name else { return }
-            fetchRequest.predicate = NSPredicate(format: "substanceName == %@", name)
-            fetchRequest.fetchLimit = 1
-            let ingestions = (try? PersistenceController.shared.viewContext.fetch(fetchRequest)) ?? []
-            if let color = ingestions.first?.substanceColor {
+            let companions = (try? PersistenceController.shared.viewContext.fetch(fetchRequest)) ?? []
+            let maybeColor = companions.first { comp in
+                comp.substanceNameUnwrapped == name
+            }?.color
+            if let color = maybeColor {
+                doesCompanionExistAlready = true
                 self.selectedColor = color
+            } else {
+                doesCompanionExistAlready = false
+                let usedColors = companions.compactMap { comp in
+                    comp.color
+                }
+                let otherColors = Set(SubstanceColor.allCases).subtracting(usedColors)
+                self.selectedColor = otherColors.first ?? SubstanceColor.allCases.randomElement() ?? SubstanceColor.blue
             }
+            isLoadingCompanions = false
         }
 
-        func addIngestion(to experience: Experience) {
+        func addIngestion(to experience: Experience?) {
             let context = PersistenceController.shared.viewContext
             context.performAndWait {
-                let ingestion = createIngestion()
-                experience.addToIngestions(ingestion)
+                if !doesCompanionExistAlready {
+                    let companion = SubstanceCompanion(context: context)
+                    companion.substanceName = substance?.name
+                    companion.colorAsText = selectedColor.rawValue
+                }
+                let ingestion = Ingestion(context: context)
+                ingestion.identifier = UUID()
+                ingestion.time = selectedTime
+                ingestion.dose = dose
+                ingestion.units = units
+                ingestion.administrationRoute = administrationRoute.rawValue
+                ingestion.substanceName = substance?.name
+                ingestion.color = selectedColor.rawValue
+                ingestion.experience = experience
                 try? context.save()
             }
-        }
-
-        func addIngestionToNewExperience() {
-            let context = PersistenceController.shared.viewContext
-            context.performAndWait {
-                let newExperience = Experience(context: context)
-                let now = Date()
-                newExperience.creationDate = now
-                newExperience.title = now.asDateString
-                let ingestion = createIngestion()
-                newExperience.addToIngestions(ingestion)
-                try? context.save()
-            }
-        }
-
-        private func createIngestion() -> Ingestion {
-            let context = PersistenceController.shared.viewContext
-            let ingestion = Ingestion(context: context)
-            ingestion.identifier = UUID()
-            ingestion.time = selectedTime
-            ingestion.dose = dose
-            ingestion.units = units
-            ingestion.administrationRoute = administrationRoute.rawValue
-            ingestion.substanceName = substance?.name
-            ingestion.color = selectedColor.rawValue
-            return ingestion
         }
     }
 }
--- PsychonautWiki Journal/Add Ingestion/Substance Sheets/Time and Color/ChooseTimeAndColor.swift
+++ PsychonautWiki Journal/Add Ingestion/Substance Sheets/Time and Color/ChooseTimeAndColor.swift
@@ -28,44 +28,19 @@ struct ChooseTimeAndColor: View {
                         ColorPicker(selectedColor: $viewModel.selectedColor)
                     }
                 }
-                if let experienceUnwrap = addIngestionContext.experience {
-                    Button("Add Ingestion") {
-                        viewModel.addIngestion(to: experienceUnwrap)
-                        dismissSheet()
-                        toastViewModel.showSuccessToast()
-                    }
-                    .buttonStyle(.primary)
-                    .padding()
-                } else {
-                    VStack {
-                        let twoDaysAgo = Date().addingTimeInterval(-2*24*60*60)
-                        if let lastExperienceUnwrap = viewModel.lastExperience,
-                           lastExperienceUnwrap.dateForSorting > twoDaysAgo {
-                            Button("Add to \(lastExperienceUnwrap.titleUnwrapped)") {
-                                viewModel.addIngestion(to: lastExperienceUnwrap)
-                                dismissSheet()
-                                toastViewModel.showSuccessToast()
-                            }
-                            .padding(.horizontal)
-                        }
-                        Button("Add to New Experience") {
-                            viewModel.addIngestionToNewExperience()
-                            dismissSheet()
-                            toastViewModel.showSuccessToast()
-                        }
-                        .padding()
-                    }
-                    .buttonStyle(.primary)
+                Button("Add Ingestion") {
+                    viewModel.addIngestion(to: addIngestionContext.experience)
+                    dismissSheet()
+                    toastViewModel.showSuccessToast()
                 }
+                .buttonStyle(.primary)
+                .padding()
             }
             .task {
                 viewModel.substance = substance
                 viewModel.administrationRoute = administrationRoute
                 viewModel.dose = dose ?? 0
                 viewModel.units = units
-                if addIngestionContext.experience == nil {
-                    viewModel.lastExperience = PersistenceController.shared.getLatestExperience()
-                }
                 viewModel.setDefaultColor()
             }
             .navigationBarTitle("Ingestion Time")
