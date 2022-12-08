a7be68c355b47667bcd4c91a3e6b11ca53f567ea
Isaak <isaak.hanimann@gmail.com>
2022-12-08 22:59:16 +0100

    Allow to deselect routes on duration screen

--- PsychonautWiki Journal/SearchTab/Substance/Section Screens/DurationScreen.swift
+++ PsychonautWiki Journal/SearchTab/Substance/Section Screens/DurationScreen.swift
@@ -11,6 +11,7 @@ struct DurationScreen: View {
     let durationInfos: [DurationInfo]
     @State private var selectedTime = Date()
     @State private var timelineModel: TimelineModel?
+    @State private var selectedRoutes: [AdministrationRoute] = []
 
     var body: some View {
         List {
@@ -30,23 +31,61 @@ struct DurationScreen: View {
                                 .font(.title2)
                                 .foregroundColor(info.route.color)
                             Text(info.route.rawValue.localizedCapitalized).font(.headline)
+                            Spacer()
+                            let isSelected = selectedRoutes.contains(info.route)
+                            Image(systemName: isSelected ? "checkmark.circle.fill" : "circle")
+                                .resizable()
+                                .frame(width: 24, height: 24)
+                                .foregroundColor(info.route.color)
+                                .onTapGesture {
+                                    toggle(route: info.route)
+                                }
                         }
-                        OneRoaDurationRow(duration: info.roaDuration, color: info.route.color)
                     }
+                    OneRoaDurationRow(duration: info.roaDuration, color: info.route.color)
                 }
-            }.onAppear {
-                timelineModel = TimelineModel(everythingForEachLine: durationInfos.map({ info in
-                    EverythingForOneLine(roaDuration: info.roaDuration, startTime: selectedTime, horizontalWeight: 0.5, verticalWeight: 1, color: info.route.color)
-                }))
             }
-            .onChange(of: selectedTime) { newValue in
-                timelineModel = TimelineModel(everythingForEachLine: durationInfos.map({ info in
-                    EverythingForOneLine(roaDuration: info.roaDuration, startTime: selectedTime, horizontalWeight: 0.5, verticalWeight: 1, color: info.route.color)
-                }))
+        }.onAppear {
+            selectAllRoutes()
+            updateModel()
+        }
+        .onChange(of: selectedTime) { _ in
+            updateModel()
+        }
+        .onChange(of: selectedRoutes) { _ in
+            updateModel()
+        }
+        .navigationTitle("Duration")
+    }
+
+    private func selectAllRoutes() {
+        selectedRoutes = durationInfos.map { info in
+            info.route
+        }
+    }
+
+    private func updateModel() {
+        let selectedDurationInfos = durationInfos.filter { info in
+            selectedRoutes.contains(info.route)
+        }
+        timelineModel = TimelineModel(everythingForEachLine: selectedDurationInfos.map({ info in
+            EverythingForOneLine(roaDuration: info.roaDuration, startTime: selectedTime, horizontalWeight: 0.5, verticalWeight: 1, color: info.route.color)
+        }))
+    }
+
+    private func toggle(route: AdministrationRoute) {
+        if selectedRoutes.contains(route) {
+            selectedRoutes.removeAll { sel in
+                sel == route
             }
-        }.navigationTitle("Duration")
+        } else {
+            selectedRoutes.append(route)
+        }
     }
 }
+
+
+
 
 struct DurationScreen_Previews: PreviewProvider {
     static var previews: some View {
