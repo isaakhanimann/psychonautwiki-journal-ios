a7d400394e8545f20eb18606d61f02b2f249a536
Isaak <isaak.hanimann@gmail.com>
2022-12-09 00:00:02 +0100

    Remove title and notes editing from experience screen

--- PsychonautWiki Journal/ExperiencesTab/Experience/ExperienceView.swift
+++ PsychonautWiki Journal/ExperiencesTab/Experience/ExperienceView.swift
@@ -3,45 +3,33 @@ import SwiftUI
 struct ExperienceView: View {
 
     @ObservedObject var experience: Experience
-    @StateObject private var viewModel = ViewModel()
     @State private var isShowingAddIngestionSheet = false
-    @Environment(\.editMode) private var editMode
 
     var body: some View {
-        let isEditing = editMode?.wrappedValue.isEditing == true
         return List {
-            if isEditing {
-                Section("Title") {
-                    TextField("Title", text: $viewModel.selectedTitle)
-                        .disableAutocorrection(true)
-                }
-            }
-            if !isEditing || !experience.sortedIngestionsUnwrapped.isEmpty {
+            if !experience.sortedIngestionsUnwrapped.isEmpty {
                 Section("Ingestions") {
                     ForEach(experience.sortedIngestionsUnwrapped) { ing in
                         IngestionRow(ingestion: ing)
-                            .deleteDisabled(!isEditing)
                     }
                     .onDelete(perform: deleteIngestions)
-                    if !isEditing {
-                        Button {
-                            isShowingAddIngestionSheet.toggle()
-                        } label: {
-                            Label("Add Ingestion", systemImage: "plus")
-                                .foregroundColor(.accentColor)
-                        }
+                    Button {
+                        isShowingAddIngestionSheet.toggle()
+                    } label: {
+                        Label("Add Ingestion", systemImage: "plus")
+                            .foregroundColor(.accentColor)
                     }
                 }
             }
-            if !viewModel.writtenText.isEmpty || isEditing {
-                Section("Notes") {
-                    if isEditing {
-                        TextEditor(text: $viewModel.writtenText)
-                            .frame(minHeight: 300)
-                    } else {
-                        Text(viewModel.writtenText)
-                            .foregroundColor(.secondary)
-                            .padding(.vertical, 5)
+            Section("Notes") {
+                if let notes = experience.textUnwrapped, !notes.isEmpty {
+                    Text(notes)
+                        .padding(.vertical, 5)
+                } else {
+                    NavigationLink {
+                        EditExperience()
+                    } label: {
+                        Label("Add Note", systemImage: "pencil")
                     }
                 }
             }
@@ -49,23 +37,14 @@ struct ExperienceView: View {
         .sheet(isPresented: $isShowingAddIngestionSheet, content: {
             ChooseSubstanceView()
         })
-        .task {
-            viewModel.initialize(experience: experience)
-        }
         .navigationTitle(experience.titleUnwrapped)
-        .onChange(of: editMode?.wrappedValue) { newValue in
-            guard let isEditing = newValue?.isEditing else {return}
-            if !isEditing {
-                hideKeyboard()
-                PersistenceController.shared.saveViewContext()
-            }
-        }
-        .onDisappear {
-            PersistenceController.shared.saveViewContext()
-        }
         .toolbar {
-            ToolbarItem(placement: .navigation) {
-                EditButton()
+            ToolbarItem(placement: .navigationBarTrailing) {
+                NavigationLink {
+                    EditExperience()
+                } label: {
+                    Label("Edit Experience", systemImage: "pencil")
+                }
             }
         }
     }
@@ -76,8 +55,5 @@ struct ExperienceView: View {
             PersistenceController.shared.viewContext.delete(ingestion)
         }
         PersistenceController.shared.saveViewContext()
-        if experience.sortedIngestionsUnwrapped.isEmpty {
-            editMode?.wrappedValue = .inactive
-        }
     }
 }
