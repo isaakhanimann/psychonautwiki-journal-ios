f87388cb365b0499af6848c34d6066bdd6bb1f1d
Isaak <isaak.hanimann@gmail.com>
2022-12-15 13:41:25 +0100

    Show suggestions of custom substances

--- PsychonautWiki Journal/Add Ingestion/Choose Substance/ChooseSubstance-ViewModel.swift
+++ PsychonautWiki Journal/Add Ingestion/Choose Substance/ChooseSubstance-ViewModel.swift
@@ -35,14 +35,6 @@ extension ChooseSubstanceScreen {
                     let allSubstances = SubstanceRepo.shared.substances
                     return SearchViewModel.getFilteredSubstancesSorted(substances: allSubstances, searchText: search)
                 }.receive(on: DispatchQueue.main).assign(to: &$filteredSubstances)
-            $filteredSubstances
-                .map { filteredSubstances in
-                    self.allPossibleSuggestions.filter { suggestion in
-                        filteredSubstances.contains { substance in
-                            substance.name == suggestion.substanceName
-                        }
-                    }
-                }.assign(to: &$filteredSuggestions)
             $searchText
                 .debounce(for: .milliseconds(100), scheduler: DispatchQueue.global(qos: .userInitiated)) // debounce because if we type extremely fast it crashes
                 .map { search in
@@ -51,6 +43,15 @@ extension ChooseSubstanceScreen {
                         custModel.name.lowercased().contains(searchLowerCased)
                     }
                 }.receive(on: DispatchQueue.main).assign(to: &$filteredCustomSubstances)
+            $filteredSubstances.combineLatest($filteredCustomSubstances) { filteredSubstances, filteredCustom in
+                self.allPossibleSuggestions.filter { suggestion in
+                    filteredSubstances.contains { substance in
+                        substance.name == suggestion.substanceName
+                    } || filteredCustom.contains { custom in
+                        custom.name == suggestion.substanceName
+                    }
+                }
+            }.assign(to: &$filteredSuggestions)
         }
     }
 }
