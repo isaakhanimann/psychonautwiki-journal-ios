1c4ad567ce4845d7dd2ff3f7cbff34c0221e4e11
Isaak <isaak.hanimann@gmail.com>
2022-12-29 15:33:55 +0100

    Show current experience distinct from others

--- PsychonautWiki Journal/Journal/ExperiencesList.swift
+++ PsychonautWiki Journal/Journal/ExperiencesList.swift
@@ -8,12 +8,29 @@ struct ExperiencesList: View {
     var body: some View {
         ZStack {
             List {
-                ForEach(viewModel.experiences) { exp in
-                    ExperienceRow(experience: exp, isTimeRelative: viewModel.isTimeRelative)
+                if let first = viewModel.experiences.first,
+                   let lastIngestionTime = first.sortedIngestionsUnwrapped.last?.time,
+                   Date().timeIntervalSinceReferenceDate - lastIngestionTime.timeIntervalSinceReferenceDate < 12*60*60 {
+                    Section("Current") {
+                        ExperienceRow(experience: first, isTimeRelative: viewModel.isTimeRelative)
+                    }
+                    let rest = viewModel.experiences.suffix(viewModel.experiences.count-1)
+                    Section("Previous") {
+                        ForEach(rest) { exp in
+                            ExperienceRow(experience: exp, isTimeRelative: viewModel.isTimeRelative)
+                        }
+                    }
+                } else {
+                    ForEach(viewModel.experiences) { exp in
+                        ExperienceRow(experience: exp, isTimeRelative: viewModel.isTimeRelative)
+                    }
                 }
             }
             if isSearching && viewModel.experiences.isEmpty {
                 Text("No Results")
+                    .foregroundColor(.secondary)
+            } else if viewModel.isFavoriteFilterEnabled && viewModel.experiences.isEmpty {
+                Text("No Favorites")
                     .foregroundColor(.secondary)
             }
         }
