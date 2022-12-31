d51a83303a36938ecbb2c54597a07f0b680d85ba
Isaak <isaak.hanimann@gmail.com>
2022-12-31 06:28:09 +0100

    Better title for timeline

--- PsychonautWiki Journal/Journal/Experience/ExperienceScreen.swift
+++ PsychonautWiki Journal/Journal/Experience/ExperienceScreen.swift
@@ -24,8 +24,6 @@ struct ExperienceScreen: View {
                         Canvas {_,_ in }.frame(height: timelineHeight)
                     }
                 } header: {
-                    Text("Effect Timeline")
-                } footer: {
                     let firstDate = experience.sortedIngestionsUnwrapped.first?.time ?? experience.sortDateUnwrapped
                     Text(firstDate, style: .date)
                 }
