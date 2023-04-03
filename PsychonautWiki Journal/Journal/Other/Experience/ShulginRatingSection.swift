e358fa547e3c0fa3b38d74e05fcf037663c76f8a
Isaak <isaak.hanimann@gmail.com>
2023-04-03 15:42:50 +0200

    Add Shulgin Rating Section

---
+++ PsychonautWiki Journal/Journal/Other/Experience/ShulginRatingSection.swift
@@ -0,0 +1,33 @@
+// Copyright (c) 2023. Isaak Hanimann.
+// This file is part of PsychonautWiki Journal.
+//
+// PsychonautWiki Journal is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public Licence as published by
+// the Free Software Foundation, either version 3 of the License, or (at 
+// your option) any later version.
+//
+// PsychonautWiki Journal is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with PsychonautWiki Journal. If not, see https://www.gnu.org/licenses/gpl-3.0.en.html.
+
+import SwiftUI
+
+struct ShulginRatingSection: View {
+    var body: some View {
+        Section("Shulgin Rating") {
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        }
+    }
+}
+
+struct ShulginRatingSection_Previews: PreviewProvider {
+    static var previews: some View {
+        List {
+            ShulginRatingSection()
+        }
+    }
+}
