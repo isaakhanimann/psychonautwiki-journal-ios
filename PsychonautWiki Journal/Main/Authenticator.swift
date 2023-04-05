78d5a7bf0eaf0d2de453337c445028eb7a90c57b
Isaak <isaak.hanimann@gmail.com>
2023-04-05 10:03:22 +0200

    Fix authentication enter code when it fails

--- PsychonautWiki Journal/Main/Authenticator.swift
+++ PsychonautWiki Journal/Main/Authenticator.swift
@@ -31,7 +31,7 @@ class Authenticator: ObservableObject {
             isStartingUp = false
             let context = LAContext()
             var error: NSError?
-            if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            if context.canEvaluatePolicy(.deviceOwnerAuthentication, error: &error) {
                 isFaceIDEnabled = true
                 // when the system shows the Face ID prompt the app moves to the background.
                 // therefore this code gets executed before it is unlocked and after
@@ -51,7 +51,7 @@ class Authenticator: ObservableObject {
 
     private func authenticate(with context: LAContext) {
         let reason = "Authenticate yourself to see your journal."
-        context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, authenticationError in
+        context.evaluatePolicy(.deviceOwnerAuthentication, localizedReason: reason) { success, authenticationError in
             if success {
                 Task { @MainActor in
                     self.isUnlocked = true
