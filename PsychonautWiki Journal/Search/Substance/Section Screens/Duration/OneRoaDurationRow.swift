96c1244de53d2546b5db20f6618ab960836c99cb
Isaak <isaak.hanimann@gmail.com>
2023-07-22 21:35:51 +0200

    Style durations screen

--- PsychonautWiki Journal/Search/Substance/Section Screens/Duration/OneRoaDurationRow.swift
+++ PsychonautWiki Journal/Search/Substance/Section Screens/Duration/OneRoaDurationRow.swift
@@ -22,36 +22,29 @@ struct OneRoaDurationRow: View {
     let color: SubstanceColor
 
     var body: some View {
-        VStack {
+        VStack(alignment: .leading) {
             HStack {
                 if let onset = duration.onset?.displayString {
                     DurationChip(name: "onset", text: onset, color: color)
-                } else {
-                    Spacer().frame(maxWidth: .infinity)
                 }
                 if let comeup = duration.comeup?.displayString {
                     DurationChip(name: "comeup", text: comeup, color: color)
-                } else {
-                    Spacer().frame(maxWidth: .infinity)
                 }
                 if let peak = duration.peak?.displayString {
                     DurationChip(name: "peak", text: peak, color: color)
-                } else {
-                    Spacer().frame(maxWidth: .infinity)
                 }
                 if let offset = duration.offset?.displayString {
                     DurationChip(name: "offset", text: offset, color: color)
-                } else {
-                    Spacer().frame(maxWidth: .infinity)
                 }
             }
             HStack {
                 if let total = duration.total?.displayString {
                     Text("total: \(total)")
+                        .colorBackground(color)
                 }
-                Spacer()
                 if let afterglow = duration.afterglow?.displayString {
                     Text("after effects: \(afterglow)")
+                        .colorBackground(color)
                 }
             }
         }
@@ -59,33 +52,23 @@ struct OneRoaDurationRow: View {
     }
 }
 
-struct DurationChip: View {
-    let name: String
-    let text: String
-    let color: SubstanceColor
-
-    var body: some View {
-        VStack {
-            Text(text)
-            Text(name)
-        }.padding(5)
-            .background(color.swiftUIColor.opacity(shapeOpacity))
-            .cornerRadius(12)
-            .frame(maxWidth: .infinity)
-    }
-}
-
 struct DurationView_Previews: PreviewProvider {
     static var previews: some View {
         NavigationView {
             List {
-                Section {
-                    OneRoaDurationRow(
-                        duration: SubstanceRepo.shared.getSubstance(name: "4-HO-MET")!.getDuration(for: .smoked)!,
-                        color: .blue
-                    )
-                }
+                OneRoaDurationRow(
+                    duration: SubstanceRepo.shared.getSubstance(name: "4-HO-MET")!.getDuration(for: .smoked)!,
+                    color: .blue
+                )
+                OneRoaDurationRow(
+                    duration: SubstanceRepo.shared.getSubstance(name: "MDMA")!.getDuration(for: .oral)!,
+                    color: .pink
+                )
+                OneRoaDurationRow(
+                    duration: SubstanceRepo.shared.getSubstance(name: "LSD")!.getDuration(for: .sublingual)!,
+                    color: .purple
+                )
             }
-        }
+        }.environmentObject(TabBarObserver())
     }
 }
