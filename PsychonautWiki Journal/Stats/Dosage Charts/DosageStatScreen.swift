c9bb8a2fc2eec4330e17afdd5ec5921948783163
Isaak <31287934+isaakhanimann@users.noreply.github.com>
2024-03-15 07:32:55 +0100

    Don't show dosage stats when there are ingestions with different units

--- PsychonautWiki Journal/Stats/Dosage Charts/DosageStatScreen.swift
+++ PsychonautWiki Journal/Stats/Dosage Charts/DosageStatScreen.swift
@@ -18,11 +18,11 @@ import SwiftUI
 import Charts
 
 struct DosageStatScreen: View {
-
+    
     let substanceName: String
     let unit: String
     let substanceColor: SubstanceColor
-
+    
     init(substanceName: String) {
         self.substanceName = substanceName
         let substance = SubstanceRepo.shared.getSubstance(name: substanceName)
@@ -45,138 +45,140 @@ struct DosageStatScreen: View {
                 NSPredicate(format: "substanceName == %@", substanceName)
             ]))
     }
-
+    
     private var ingestions: FetchRequest<Ingestion>
     @State private var unknownDoseEstimate = 0.0
-
+    
     enum StatTimeRangeOption: String, CaseIterable {
         case last30Days = "30D"
         case last26Weeks = "26W"
         case last12Months = "12M"
         case allYears = "Y"
     }
-
+    
     @State private var selectedTimeRangeOption = StatTimeRangeOption.last26Weeks
-
+    
     var body: some View {
         List {
-            Section {
-                Picker("Time range", selection: $selectedTimeRangeOption) {
-                    ForEach(StatTimeRangeOption.allCases, id: \.rawValue) { timeRange in
-                        Text(timeRange.rawValue).tag(timeRange)
-                    }
-                }.pickerStyle(.segmented)
-
-                switch selectedTimeRangeOption {
-                case .last30Days:
-                    if let last30Days = dosageStat?.last30Days, !last30Days.isEmpty {
-                        VStack(alignment: .leading) {
-                            VStack(alignment: .leading) {
-                                Text("Dosage by day")
-                                    .font(.callout)
-                                    .foregroundStyle(.secondary)
-                                Text("Last 30 Days")
-                                    .font(.title2.bold())
-                                    .foregroundColor(.primary)
-                            }
-                            Chart(last30Days, id: \.day) {
-                                BarMark(
-                                    x: .value("Day", $0.day, unit: .day),
-                                    y: .value("Dosage", $0.dosage)
-                                )
-                                .foregroundStyle(substanceColor.swiftUIColor)
-                            }.chartYAxisLabel("Dosage in \(unit)")
-                                .frame(height: 240)
+            if doAllIngestionsHaveSameUnitEqualToSubstance {
+                Section {
+                    Picker("Time range", selection: $selectedTimeRangeOption) {
+                        ForEach(StatTimeRangeOption.allCases, id: \.rawValue) { timeRange in
+                            Text(timeRange.rawValue).tag(timeRange)
                         }
-                    } else {
-                        Text("No \(substanceName) ingestions in the last 30 days").foregroundStyle(.secondary)
-                    }
-                case .last26Weeks:
-                    if let last26Weeks = dosageStat?.last26Weeks, !last26Weeks.isEmpty {
-                        VStack(alignment: .leading) {
+                    }.pickerStyle(.segmented)
+                    
+                    switch selectedTimeRangeOption {
+                    case .last30Days:
+                        if let last30Days = dosageStat?.last30Days, !last30Days.isEmpty {
                             VStack(alignment: .leading) {
-                                Text("Dosage by week")
-                                    .font(.callout)
-                                    .foregroundStyle(.secondary)
-                                Text("Last 26 Weeks")
-                                    .font(.title2.bold())
-                                    .foregroundColor(.primary)
+                                VStack(alignment: .leading) {
+                                    Text("Dosage by day")
+                                        .font(.callout)
+                                        .foregroundStyle(.secondary)
+                                    Text("Last 30 Days")
+                                        .font(.title2.bold())
+                                        .foregroundColor(.primary)
+                                }
+                                Chart(last30Days, id: \.day) {
+                                    BarMark(
+                                        x: .value("Day", $0.day, unit: .day),
+                                        y: .value("Dosage", $0.dosage)
+                                    )
+                                    .foregroundStyle(substanceColor.swiftUIColor)
+                                }.chartYAxisLabel("Dosage in \(unit)")
+                                    .frame(height: 240)
                             }
-                            Chart(last26Weeks, id: \.week) {
-                                BarMark(
-                                    x: .value("Week", $0.week, unit: .weekOfYear),
-                                    y: .value("Dosage", $0.dosage)
-                                )
-                                .foregroundStyle(substanceColor.swiftUIColor)
-                            }.chartYAxisLabel("Dosage in \(unit)")
-                                .frame(height: 240)
+                        } else {
+                            Text("No \(substanceName) ingestions in the last 30 days").foregroundStyle(.secondary)
                         }
-                    } else {
-                        Text("No \(substanceName) ingestions in the last 26 weeks").foregroundStyle(.secondary)
-                    }
-                case .last12Months:
-                    if let last12Months = dosageStat?.last12Months, !last12Months.isEmpty {
-                        VStack(alignment: .leading) {
+                    case .last26Weeks:
+                        if let last26Weeks = dosageStat?.last26Weeks, !last26Weeks.isEmpty {
                             VStack(alignment: .leading) {
-                                Text("Dosage by month")
-                                    .font(.callout)
-                                    .foregroundStyle(.secondary)
-                                Text("Last 12 Months")
-                                    .font(.title2.bold())
-                                    .foregroundColor(.primary)
+                                VStack(alignment: .leading) {
+                                    Text("Dosage by week")
+                                        .font(.callout)
+                                        .foregroundStyle(.secondary)
+                                    Text("Last 26 Weeks")
+                                        .font(.title2.bold())
+                                        .foregroundColor(.primary)
+                                }
+                                Chart(last26Weeks, id: \.week) {
+                                    BarMark(
+                                        x: .value("Week", $0.week, unit: .weekOfYear),
+                                        y: .value("Dosage", $0.dosage)
+                                    )
+                                    .foregroundStyle(substanceColor.swiftUIColor)
+                                }.chartYAxisLabel("Dosage in \(unit)")
+                                    .frame(height: 240)
                             }
-                            Chart(last12Months, id: \.month) {
-                                BarMark(
-                                    x: .value("Month", $0.month, unit: .month),
-                                    y: .value("Dosage", $0.dosage)
-                                )
-                                .foregroundStyle(substanceColor.swiftUIColor)
-                            }.chartYAxisLabel("Dosage in \(unit)")
-                                .frame(height: 240)
+                        } else {
+                            Text("No \(substanceName) ingestions in the last 26 weeks").foregroundStyle(.secondary)
                         }
-                    } else {
-                        Text("No \(substanceName) ingestions in the last 12 months").foregroundStyle(.secondary)
-                    }
-                case .allYears:
-                    if let years = dosageStat?.years, !years.isEmpty {
-                        VStack(alignment: .leading) {
+                    case .last12Months:
+                        if let last12Months = dosageStat?.last12Months, !last12Months.isEmpty {
                             VStack(alignment: .leading) {
-                                Text("Dosage by year")
-                                    .font(.callout)
-                                    .foregroundStyle(.secondary)
-                                Text("All Years")
-                                    .font(.title2.bold())
-                                    .foregroundColor(.primary)
+                                VStack(alignment: .leading) {
+                                    Text("Dosage by month")
+                                        .font(.callout)
+                                        .foregroundStyle(.secondary)
+                                    Text("Last 12 Months")
+                                        .font(.title2.bold())
+                                        .foregroundColor(.primary)
+                                }
+                                Chart(last12Months, id: \.month) {
+                                    BarMark(
+                                        x: .value("Month", $0.month, unit: .month),
+                                        y: .value("Dosage", $0.dosage)
+                                    )
+                                    .foregroundStyle(substanceColor.swiftUIColor)
+                                }.chartYAxisLabel("Dosage in \(unit)")
+                                    .frame(height: 240)
                             }
-                            Chart(years, id: \.year) {
-                                BarMark(
-                                    x: .value("Year", $0.year, unit: .year),
-                                    y: .value("Dosage", $0.dosage)
-                                )
-                                .foregroundStyle(substanceColor.swiftUIColor)
-                            }.chartYAxisLabel("Dosage in \(unit)")
-                                .frame(height: 240)
+                        } else {
+                            Text("No \(substanceName) ingestions in the last 12 months").foregroundStyle(.secondary)
                         }
-                    } else {
-                        Text("No \(substanceName) ingestions").foregroundStyle(.secondary)
+                    case .allYears:
+                        if let years = dosageStat?.years, !years.isEmpty {
+                            VStack(alignment: .leading) {
+                                VStack(alignment: .leading) {
+                                    Text("Dosage by year")
+                                        .font(.callout)
+                                        .foregroundStyle(.secondary)
+                                    Text("All Years")
+                                        .font(.title2.bold())
+                                        .foregroundColor(.primary)
+                                }
+                                Chart(years, id: \.year) {
+                                    BarMark(
+                                        x: .value("Year", $0.year, unit: .year),
+                                        y: .value("Dosage", $0.dosage)
+                                    )
+                                    .foregroundStyle(substanceColor.swiftUIColor)
+                                }.chartYAxisLabel("Dosage in \(unit)")
+                                    .frame(height: 240)
+                            }
+                        } else {
+                            Text("No \(substanceName) ingestions").foregroundStyle(.secondary)
+                        }
+                    }
+                }.listRowSeparator(.hidden)
+                
+                if areThereUnknownDoses {
+                    Section("Estimate unknown doses as") {
+                        HStack  {
+                            TextField(
+                                "Unknown dose estimate",
+                                value: $unknownDoseEstimate,
+                                format: .number).keyboardType(.decimalPad)
+                            Text(unit)
+                        }
                     }
                 }
-            }.listRowSeparator(.hidden)
-
-            if areThereUnknownDoses {
-                Section("Estimate unknown doses as") {
-                    HStack  {
-                        TextField(
-                            "Unknown dose estimate",
-                            value: $unknownDoseEstimate,
-                            format: .number).keyboardType(.decimalPad)
-                        Text(unit)
-                    }
+            } else {
+                Section {
+                    Text("Not all your ingestions of \(substanceName) are using \(unit) as their pure unit. It's therefore not possible to sum those doses together to show a graph.")
                 }
-            }
-
-            Section {
-                Text("Todo check if all ingestions have same unit and  if not then don't show graph. If different administration routes are used add a note saying that.")
             }
         }
         .navigationTitle(substanceName)
@@ -190,33 +192,37 @@ struct DosageStatScreen: View {
             updateStats()
         }
     }
-
+    
     @State private var dosageStat: DosageStat?
     @State private var areThereUnknownDoses = false
-
+    @State private var doAllIngestionsHaveSameUnitEqualToSubstance = true
+    
     struct DoseInstance {
         let date: Date
         let dose: Double
     }
-
+    
     private func updateStats() {
+        doAllIngestionsHaveSameUnitEqualToSubstance = ingestions.wrappedValue.allSatisfy({ ing in
+            ing.pureUnits == unit
+        })
+        areThereUnknownDoses = ingestions.wrappedValue.contains(where: { ing in
+            ing.doseUnwrapped == nil
+        })
         dosageStat = DosageStat(
             last30Days: getDayDosages(),
             last26Weeks: getWeekDosages(),
             last12Months: getMonthDosages(),
             years: getYearDosages())
-        areThereUnknownDoses = ingestions.wrappedValue.contains(where: { ing in
-            ing.doseUnwrapped == nil
-        })
     }
-
+    
     private func getDayDosages() -> [DayDosage] {
         let last30Days = ingestions.wrappedValue.prefix { ing in
             Calendar.current.numberOfDaysBetween(ing.timeUnwrapped, and: .now) <= 30
         }
-
+        
         let doseInstances = convertToDoseInstances(ingestions: last30Days)
-
+        
         let doseInstancesGroupedByDay = Dictionary(grouping: doseInstances) { instanceDose in
             let components = Calendar.current.dateComponents([.day, .month], from: instanceDose.date)
             if let day = components.day, let month = components.month {
@@ -225,7 +231,7 @@ struct DosageStatScreen: View {
                 return ""
             }
         }.values
-
+        
         let dayDosages = doseInstancesGroupedByDay.compactMap { (doseInstances: [DoseInstance]) in
             if let summedUp = sumUpInstanceDosages(doseInstances: doseInstances) {
                 return DayDosage(day: summedUp.date, dosage: summedUp.dose)
@@ -235,14 +241,14 @@ struct DosageStatScreen: View {
         }
         return dayDosages
     }
-
+    
     private func getWeekDosages() -> [WeekDosage] {
         let last26Weeks = ingestions.wrappedValue.prefix { ing in
             Calendar.current.numberOfDaysBetween(ing.timeUnwrapped, and: .now) <= 26*7
         }
-
+        
         let doseInstances = convertToDoseInstances(ingestions: last26Weeks)
-
+        
         let doseInstancesGroupedByWeek = Dictionary(grouping: doseInstances) { instanceDose in
             let components = Calendar.current.dateComponents([.weekOfYear], from: instanceDose.date)
             if let weekOfYear = components.weekOfYear {
@@ -251,7 +257,7 @@ struct DosageStatScreen: View {
                 return ""
             }
         }.values
-
+        
         let weekDosages = doseInstancesGroupedByWeek.compactMap { (doseInstances: [DoseInstance]) in
             if let summedUp = sumUpInstanceDosages(doseInstances: doseInstances) {
                 return WeekDosage(week: summedUp.date, dosage: summedUp.dose)
@@ -261,14 +267,14 @@ struct DosageStatScreen: View {
         }
         return weekDosages
     }
-
+    
     private func getMonthDosages() -> [MonthDosage] {
         let last12Months = ingestions.wrappedValue.prefix { ing in
             Calendar.current.numberOfDaysBetween(ing.timeUnwrapped, and: .now) <= 365
         }
-
+        
         let instanceDosages = convertToDoseInstances(ingestions: last12Months)
-
+        
         let doseInstancesGroupedByMonth = Dictionary(grouping: instanceDosages) { instanceDose in
             let components = Calendar.current.dateComponents([.month, .year], from: instanceDose.date)
             if let month = components.month, let year = components.year {
@@ -277,7 +283,7 @@ struct DosageStatScreen: View {
                 return ""
             }
         }
-
+        
         let monthDosages = doseInstancesGroupedByMonth.values.compactMap { (doseInstances: [DoseInstance]) in
             if let summedUp = sumUpInstanceDosages(doseInstances: doseInstances) {
                 return MonthDosage(month: summedUp.date, dosage: summedUp.dose)
@@ -287,10 +293,10 @@ struct DosageStatScreen: View {
         }
         return monthDosages
     }
-
+    
     private func getYearDosages() -> [YearDosage] {
         let doseInstances = convertToDoseInstances(ingestions: ingestions.wrappedValue)
-
+        
         let doseInstancesGroupedByYear = Dictionary(grouping: doseInstances) { instanceDose in
             let components = Calendar.current.dateComponents([.year], from: instanceDose.date)
             if let year = components.year {
@@ -299,7 +305,7 @@ struct DosageStatScreen: View {
                 return ""
             }
         }.values
-
+        
         let yearDosages = doseInstancesGroupedByYear.compactMap { (doseInstances: [DoseInstance]) in
             if let summedUp = sumUpInstanceDosages(doseInstances: doseInstances) {
                 return YearDosage(year: summedUp.date, dosage: summedUp.dose)
@@ -309,7 +315,7 @@ struct DosageStatScreen: View {
         }
         return yearDosages
     }
-
+    
     private func sumUpInstanceDosages(doseInstances: [DoseInstance]) -> DoseInstance? {
         let summedDosage = doseInstances.map({$0.dose}).reduce(0.0, +)
         if let date = doseInstances.first?.date, summedDosage > 0 {
@@ -318,7 +324,7 @@ struct DosageStatScreen: View {
             return nil
         }
     }
-
+    
     private func convertToDoseInstances(ingestions: any Collection<Ingestion>) -> [DoseInstance] {
         return ingestions.map { ing in
             DoseInstance(date: ing.timeUnwrapped, dose: ing.pureSubstanceDose ?? unknownDoseEstimate)
